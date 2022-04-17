79e0ad88be452e87351c679a45abd59741c788f2
Natali <fonatka@gmail.com>
2022-04-18 00:25:27 +0300

    Task 2 completed

--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/* 
 Console.Write("Imput first integer number: ");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Imput second integer number: ");
@@ -11,4 +12,30 @@ if (num1 > num2)
 else
 {
     Console.WriteLine("max = " + num2);
+}
+*/
+
+//Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Imput first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput third integer number: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2 && num1 > num3)
+{
+    Console.WriteLine("max = " + num1);
+}
+else
+{
+    if (num2 > num3)
+    {
+        Console.WriteLine("max = " + num2);
+    }
+    else 
+    {
+        Console.WriteLine("max = " + num3);
+    }
 }
