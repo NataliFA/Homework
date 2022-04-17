0f0aa47f0a6460f081b072b9dd809b1a809a9570
Natali <fonatka@gmail.com>
2022-04-18 00:09:34 +0300

    Task 1 completed

--- Homework_001/Program.cs
+++ Homework_001/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Imput first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Imput second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+}
